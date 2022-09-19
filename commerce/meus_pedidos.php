3df5fed8ddd9fe6fbeb0e843d81db95e9c0bcba7
Lucas_Bressan <lucas.bressan@unidavi.edu.br>
2022-09-19 17:14:01 -0300

    meus pedidos

--- commerce/meus_pedidos.php
+++ commerce/meus_pedidos.php
@@ -7,8 +7,47 @@
     $pedidos->execute(array('usuario_id'=>$_SESSION['usuario']['id']));
 
     while($linha = $pedidos->fetch()) {
-        print_r(($linha));
-        echo '<br>';
+        echo "<h5>#".$linha['id']." - ".$linha['data_venda']."</h5>";
+
+
+        $sql_itens = "SELECT * from vendas_produtos v inner join produtos p on(p.id = v.produto_id) where v.venda_id = :venda_id";
+        $item = $conn->prepare($sql_itens);
+        $item->execute(array('venda_id'=>$linha['id']));
+        ?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#ID</th>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            <?php
+            $total = 0;
+            while($linha_item = $item->fetch()) { 
+                $total += $linha_item['valor'];
+                ?>
+                <tr>
+                    <th scope="row"><?php echo $linha_item['id']; ?></th>
+                    <td><?php echo $linha_item['produto_id']; ?></td>
+                    <td><?php echo $linha_item['nome']; ?></td>
+                    <td><?php echo $linha_item['valor']; ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+            <tr>
+                <td>
+                    Total: <?php echo $total; ?>
+                </td>
+            </tr>
+            <br>
+            </tbody>
+        </table>
+        <?php
     }
 
 ?>
--- commerce/realizar_pedido.php
+++ commerce/realizar_pedido.php
@@ -1,27 +1,28 @@
-<h3 class="text-center" style="text-transform: uppercase; color: #2a6f97">Realizar o Pedido</h3>
-
-<?php
- if(isset($_POST['gravar'])) {
-    $sql_vendas = "INSERT into vendas(usuario_id, data_venda) values (:id, now())";
-    $vendas = $conn->prepare($sql_vendas);
-    $vendas->execute(array('id'=>$_SESSION['usuario']['id']));
-
-     $venda_id = $conn->lastInsertId();
-
-     foreach($_SESSION['carrinho'] as $item) {
-        $sql_item = "INSERT into vendas_produtos(venda_id, produto_id, valor_venda) values (:venda_id, :produto_id, (select valor from produtos where id = :produto_id))";
-        $insert_item = $conn->prepare($sql_item);
-        $insert_item->execute(array("venda_id"=>$venda_id, "produto_id"=>$item[0]));
-     }
-     
-     unset($_SESSION['carrinho']);
- } else {
-    ?>
-    <form method="post">
-        <div class="text-center">
-            <input type="submit" class="btn btn-primary botoes-principais" name="gravar" value="Confirmar Pedido">
-        </div>
-    </form>
+<div class="pg-realizar-pedido">
+    <div class="text-center"><img src="img/dn.png"  width="200"></div><br><br>
+    <h2 class="text-center" style="text-transform: uppercase; color: #2a6f97">Realizar o Pedido</h2><br><br>
     <?php
- }
-?>
+    if(isset($_POST['gravar'])) {
+        $sql_vendas = "INSERT into vendas(usuario_id, data_venda) values (:id, now())";
+        $vendas = $conn->prepare($sql_vendas);
+        $vendas->execute(array('id'=>$_SESSION['usuario']['id']));
+
+        $venda_id = $conn->lastInsertId();
+
+        foreach($_SESSION['carrinho'] as $item) {
+            $sql_item = "INSERT into vendas_produtos(venda_id, produto_id, valor_venda) values (:venda_id, :produto_id, (select valor from produtos where id = :produto_id))";
+            $insert_item = $conn->prepare($sql_item);
+            $insert_item->execute(array("venda_id"=>$venda_id, "produto_id"=>$item[0]));
+        }
+        unset($_SESSION['carrinho']);
+    } else {
+        ?>
+        <form method="post">
+            <div class="text-center">
+                <input type="submit" class="btn btn-primary botoes-principais" name="gravar" value="Confirmar Pedido">
+            </div>
+        </form>
+        <?php
+    }
+    ?>
+</div>
